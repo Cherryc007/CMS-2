1cdfc5d6264cf7ebd042ce196373adfbe580cbee
vanita006 <vanitakaur006@gmail.com>
2025-04-09 10:26:59 +0530

    db error

--- src/app/api/admin/papers/[Id]/route.js
+++ src/app/api/admin/papers/[Id]/route.js
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { connectToDatabase } from "@/lib/db";
-import Paper from "@/models/Paper";
+import connectDB from "@/lib/connectDB";
+import Paper from "@/models/paperModel";
 
 export async function GET(request, { params }) {
   try {
@@ -22,7 +22,7 @@ export async function GET(request, { params }) {
       );
     }
 
-    await connectToDatabase();
+    await connectDB();
 
     const paper = await Paper.findById(id)
       .populate("author", "name email")
